32e92e44a9def7327900b4c5f9d06671945baea2
Hashir Shakeel <misterperfect970@gmail.com>
2025-10-15 21:26:31 +0500

    Update project: fixed routing and prepared for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Pixels/',   // ✅ Add this line
   plugins: [react(), tailwindcss()],
 })
